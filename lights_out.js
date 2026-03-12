81f78a2afe8fe69d8d6774c0e33bc2964d38952b
roboyeti <roboyeti@gmail.com>
2026-03-12 06:51:01 -0700

    * Added rough audio and stub ins * Added board lock out between games * More work into init options (still not completely done) * Clean up * Index page imporvements for phone and in general

--- lights_out.js
+++ lights_out.js
@@ -6,11 +6,38 @@ class LightsOutScene extends Phaser.Scene {
       grid_size: config.grid_size || 3,
       tile_spacing: 8,
       move_count: null,
-      win_action: function(t) { t.winText.setVisible(true); },
       type: 'lights_out',
+      light_on_image: 'lights_on.png',
+      light_off_image: 'lights_off.png',
+      image_path: './',
+      audio_path: './',
+      sounds: {
+        move: 'lights_out_move_b.mp3',
+        // move2: 'lights_out_move2.mp3',
+        start: 'lights_out_start_b.mp3',
+        win: 'lights_out_win.mp3',
+      },
+      enable_restart: true,
+      enable_reroll: true,
+      enable_clear: true,
+      audio_engine: null,
+      win_action: function(t) { t.winText.setVisible(true); },
+      on_move_audio: function() { 
+        //console.log(this)
+        //console.log(this.audio_engine.play_sound_unqiue)
+        this.play_sound_unqiue('move',{ detune: 200 }) 
+        // this.play_sound_unqiue('move2',{ detune: 100 }) 
+      }.bind(this),
+      on_win_audio: function() { this.play_sound('win') }.bind(this),
+      on_start_audio: function() { this.play_sound('start') }.bind(this),
       ...config
     }
     // console.log(this.config)
+    window.lightsout_loaded = {}
+    this.audio_keys = []
+    this.plugins = {}
+    this.config.modules_path = '.'
+    this.key = 'lightsout'
     if (!this.config.move_count) { this.config.move_count = this.config.grid_size * 2; }
     if (this.config.grid_size > 9) { this.config.grid_size = 9 }
     if (this.config.grid_size < 3) { this.config.grid_size = 3 }
@@ -32,24 +59,50 @@ class LightsOutScene extends Phaser.Scene {
     this.lights_off = true
     this.buttons = []
     this.moves = []
+    if (this.config.audio_engine == null) {
+      this.load_script('puzzleaudio',()=> {
+        this.audio_engine = new Puzzleaudio()
+        this.game.scene.add('audio_engine', this.audio_engine, true, {} );
+        this.game.audio_engine = this.audio_engine
+      })
+    }
   }
 
   preload() {
+    this.load.setPath(this.config.image_path);
     if (this.config.background_image) {
         this.load.image('background', this.config.background_image);
     }
 
     // Optional: Load assets if using custom sprites
-    if (this.config.tile_swap) {
-      this.load.image('light_off', './lights_on.png');
-      this.load.image('light_on', './lights_off.png');
-    } else {
-      this.load.image('light_on', './lights_on.png');
-      this.load.image('light_off', './lights_off.png');      
+    if (this.config.light_on_image) {
+      if (this.config.tile_swap) {
+        this.load.image('light_off', this.config.light_on_image);
+        this.load.image('light_on', this.config.light_off_image);
+      } else {
+        this.load.image('light_on', this.config.light_on_image);
+        this.load.image('light_off', this.config.light_off_image);      
+      }
     }
+
+    this.load.setPath(this.config.audio_path);
+    Object.entries(this.config.sounds).forEach(([key, value]) => {
+      if (value) { 
+        this.load.audio(`lightsout_${key}_snd`,value);
+        console.log(`lightsout_${key}_snd`,value)
+      }
+    })
   }
 
   create() {
+    this.game.plugins = this.plugins
+    Object.entries(this.config.sounds).forEach(([key, value]) => { 
+      if (value) { 
+        this.audio_keys.push(key)
+        this.audio_engine.add_sound(`lightsout_${key}_snd`,this.key)
+      }
+    })
+
     if (this.config.background_image) {
         this.background = this.add.image(0, 0, 'background')
           .setOrigin(0, 0)
@@ -98,8 +151,10 @@ class LightsOutScene extends Phaser.Scene {
           this.overlay[y][x] = this.add.graphics();
         // }
 
-        tile.setInteractive();
-        tile.on('pointerdown', () => this.tileClick(x,y));
+        tile.on('pointerdown', () => {
+          this.tileClick(x,y)
+          this.config.on_move_audio()
+        });
 
         this.tiles[y][x] = tile;
       }
@@ -108,38 +163,58 @@ class LightsOutScene extends Phaser.Scene {
 
     // Add win text (hidden initially)
     this.winText = this.add.text(this.board_width/2, this.board_height/2, 'You win!', {
-      fontSize: '20px',
+      fontSize: '30px',
       fill: '#00ff00',
       backgroundColor: '#000000',
       fontStyle: 'bold'
-    }).setOrigin(0.9);
+    }).setOrigin(0.5,0.5);
     this.winText.setVisible(false);
 
-    this.addButton(
-      this.config.tile_spacing, 
-      this.board_height - 40, 8, 
-      'restart', 'RESTART',
-      () => this.resetBoard()
-    );
+    if (this.config.enable_restart) {
+      this.addButton(
+        this.config.tile_spacing, 
+        this.board_height - 40, 8, 'restart', 'RESTART',
+        () => this.resetBoard()
+      );
+    }
 
-    this.addButton(
-      this.config.tile_spacing + (this.board_width/3), 
-      this.board_height - 40, 8, 
-      'reroll', 'RE-ROLL',
-      () => this.createBoard(this.config.move_count)
-    );
+    if (this.config.enable_restart) {
+      this.addButton(
+        this.config.tile_spacing + (this.board_width/3), 
+        this.board_height - 40, 8, 'reroll', 'RE-ROLL',
+        () => this.createBoard(this.config.move_count)
+      );
+    }
 
-    this.addButton(
-      this.config.tile_spacing + (this.board_width/3) * 2, 
-      this.board_height - 40, 8, 
-      'clear', 'CLEAR',
-      () => this.clearBoard()
-    );
+    if (this.config.enable_restart) {
+      this.addButton(
+        this.config.tile_spacing + (this.board_width/3) * 2, 
+        this.board_height - 40, 8, 'clear', 'CLEAR',
+        () => this.clearBoard()
+      );
+    }
 
     this.createBoard(this.config.move_count);
   }
 
+  disableLights() {
+    for (let y = 0; y < this.config.grid_size; y++) {
+      for (let x = 0; x < this.config.grid_size; x++) {
+        this.tiles[y][x].disableInteractive()
+      }
+    }
+  }
+
+  enableLights() {
+    for (let y = 0; y < this.config.grid_size; y++) {
+      for (let x = 0; x < this.config.grid_size; x++) {
+        this.tiles[y][x].setInteractive();
+      }
+    }
+  }
+
   addButton(x,y,pad,name,text,ondown=function(){}) {
+    // this.add.roundRectangle(0, 0, 300, 150, 20, 0x222222);
     this.buttons[name] = this.add.text(
       x, 
       y, 
@@ -148,10 +223,12 @@ class LightsOutScene extends Phaser.Scene {
         fontStyle: 'bold',
         fill: '#ddaa00',
         backgroundColor: '#000000',
-        padding: { x: pad, y: pad }
+        //stroke: '#ddaa00',
+        //strokeThickness: 2,
+        padding: { x: pad, y: pad/2 }
       }
     ).setOrigin(0,0).setInteractive().setAlpha(0.95);
-
+    this.buttons[name].preFX.addGlow(0Xddaa00, 8, 0, false);
     this.buttons[name].on('pointerdown', ondown);
   }
 
@@ -171,9 +248,8 @@ class LightsOutScene extends Phaser.Scene {
   }
 
   createBoard(move_count=10) {
+    this.disableLights()
     this.winText.setVisible(false);
-    // console.log(JSON.stringify(this.grid))
-    // console.log(JSON.stringify(this.source_grid))
     this.moves = []
     this.clearBoard()
 
@@ -181,11 +257,6 @@ class LightsOutScene extends Phaser.Scene {
       for (let x = 0; x < this.config.grid_size; x++) {
         this.source_grid[y][x] = false; 
         this.patterns[y][x] = this.config.pattern_action()
-        // if (this.config.type == 'flip') {
-        //   this.patterns[y][x] = this.generateRandomPatterns();
-        // } else {
-        //   this.patterns[y][x] = this.normalPattern();
-        // }
         this.overlay[y][x].clear()
         if (this.config.show_pattern) {
           this.setOverlay(x,y)
@@ -200,20 +271,18 @@ class LightsOutScene extends Phaser.Scene {
         x = Phaser.Math.Between(0, (this.config.grid_size - 1))
         y = Phaser.Math.Between(0, (this.config.grid_size - 1))
       } while(x == last_x && y == last_y)
-      // console.log(y,last_y,x,last_x)
       last_x = x 
       last_y = y 
-    // console.log(m + ': ' + x + ',' + y)
       this.moves.unshift([x+1,y+1])
       this.toggleTile(x, y, true)
     }
-
+    this.enableLights()
     this.winnable = true
+    this.config.on_start_audio()
   }
 
   tileClick(x,y) {
     this.toggleTile(x,y)
-    // Check win condition
     this.checkWin();
   }
 
@@ -295,6 +364,8 @@ class LightsOutScene extends Phaser.Scene {
     if (this.winnable == true) {
       const allOff = this.grid.every(row => row.every(light => !light));
       if (allOff) {
+        this.disableLights()
+        this.config.on_win_audio()
         this.config.win_action(this)
       }
     }
@@ -310,7 +381,6 @@ class LightsOutScene extends Phaser.Scene {
     }
     if (over && this.config.type == 'flip') {
       this.setOverlay(x,y)
-      // this.overlay[y][x]
     }
   }
 
@@ -322,7 +392,9 @@ class LightsOutScene extends Phaser.Scene {
         this.updateTile(x,y)
       }
     }
+    this.enableLights()
     this.winnable = true
+    this.config.on_start_audio()
   }
 
   clearBoard() {
@@ -332,28 +404,65 @@ class LightsOutScene extends Phaser.Scene {
         this.updateTile(x,y,false)
       }
     }
+    this.disableLights()
     this.winnable = false
     this.winText.setVisible(false);
   }
 
-  toggleTile_LO(x, y,source=false) {
-    // Toggle the clicked tile and its neighbors
-    const neighbors = [
-      { dx: 0, dy: 0 }, // self
-      { dx: 0, dy: -1 }, // up
-      { dx: 0, dy: 1 },  // down
-      { dx: -1, dy: 0 }, // left
-      { dx: 1, dy: 0 }   // right
-    ];
+  load_script(name,onload=null) {
+    if (this.config.preload) { window.lightsout_loaded[name] = true; }
+    // else {
+      // if (!window.lightsout_loaded[name]) {
+      //   this.load_script(name, ()=>{
+           this.insert_script(name,onload)
+      //   })
+      // }
+//      else { this.insert_script(name,onload) }
+    // }
+  }
 
-    for (const { dx, dy } of neighbors) {
-      const nx = x + dx;
-      const ny = y + dy;
-      if (nx >= 0 && nx < this.config.grid_size && ny >= 0 && ny < this.config.grid_size) {
-        this.grid[ny][nx] = !this.grid[ny][nx];
-        if (source) { this.source_grid[ny][nx] = this.grid[ny][nx] }
-        this.updateTile(nx,ny)
+  insert_script(name,onload=()=>{ }) {
+    let min = ''
+    if (this.config.minimized_scripts) {
+      min = '.min'
+    }
+    if (window.lightsout_loaded[name]) {
+      onload.call(this)
+    } else {
+      let script = document.createElement('script');
+      script.id = `${name}.js`;
+      script.src = `${this.config.modules_path}/${name}${min}.js`;
+      document.body.append(script);
+      script.onload = ()=> { 
+        window.lightsout_loaded[name] = true; 
+        onload.call(this);  
+        this.config.debug && console.log(`${name} loaded.`); 
       }
     }
   }
+  play_sound(key,options={}) {
+    this.audio_engine.play_sound(`${this.key}_${key}_snd`,options)
+  }
+  play_sound_unqiue(key,options={}) {
+    this.audio_engine.play_sound_unique(`${this.key}_${key}_snd`,options)
+  }
+
+  stop_sounds() {
+    this.audio_engine.stop_bank(this.key)
+  }
+
+
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function random_item(collection) {
+    if (Array.isArray(collection)) {
+      return collection[Math.floor(Math.random()*collection.length)];
+    } else {
+      let oe = Object.entries(collection)
+      return oe[Math.floor(Math.random()*oe.length)][1];      
+    }
 }
